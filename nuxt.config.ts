4293fbb3780802eac301190867a28db6733ea272
riya <ramune.remonedo@gmail.com>
2024-10-17 17:40:01 +0900

    Add pageTransition & routeRules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,16 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui', '@nuxt/image']
+  modules: ['@nuxt/ui', '@nuxt/image'],
+  app: {
+    pageTransition: { name: 'page', mode: 'out-in' }
+  },
+  routeRules: {
+    '/': {
+      prerender: true,
+    },
+    '/members': {
+      prerender: true
+    }
+  }
 })
