c1df3b9b7569025a01d0c5e7d9e63f7eafc916c5
riya <ramune.remonedo@gmail.com>
2024-10-17 17:40:18 +0900

    Add SEO meta

--- app.vue
+++ app.vue
@@ -1,4 +1,12 @@
 <script setup lang="ts">
+useSeoMeta({
+  title: 'Team はくちゅーむ | 公式サイト | ' + useRoute().meta.name,
+  description: 'Team はくちゅーむはチーム内で開発したプロジェクトを公開・提供する活動をしているチームです！',
+  ogTitle: 'Team はくちゅーむ | 公式サイト',
+  ogDescription: 'Team はくちゅーむはチーム内で開発したプロジェクトを公開・提供する活動をしているチームです！',
+  ogImage: '/icon.webp',
+  twitterCard: 'summary_large_image',
+})
 </script>
 
 <template>
